01e0c4e6b639d66e7bde084497b2c412fbb4e5d5
HL <anotherdayinpara@yahoo.com>
2010-06-01 16:25:40 -0400

    added many-to-many relationship mapping

--- src/main/java/training/hl/bean/hibernate/User.java
+++ src/main/java/training/hl/bean/hibernate/User.java
@@ -13,6 +13,9 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import lombok.Data;
@@ -22,29 +25,32 @@ import training.hl.bean.hibernate.enums.Gender;
 
 @Data
 @Entity
-@EqualsAndHashCode(callSuper=false,exclude={"posts", "categories"})
-@ToString(callSuper=false, exclude={"posts", "categories"})
+@EqualsAndHashCode(callSuper = false, exclude = { "posts", "categories", "roles" })
+@ToString(callSuper = false, exclude = { "posts", "categories", "roles" })
 public class User extends RootEntity
 {
 	private static final long serialVersionUID = 5840474281304089091L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private int age;
 	@Enumerated(EnumType.ORDINAL)
 	private Gender gender;
-	@Column(insertable=false, updatable=false)
+	@Column(insertable = false, updatable = false)
 	private Date createdDate;
-	@OneToMany(mappedBy="user", cascade={ CascadeType.PERSIST, CascadeType.MERGE}, fetch=FetchType.EAGER)
+	@OneToMany(mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
 	private Set<Post> posts = new HashSet<Post>();
-	@OneToMany(mappedBy="user", cascade={ CascadeType.ALL})
+	@OneToMany(mappedBy = "user", cascade = { CascadeType.ALL })
 	private Set<Category> categories = new HashSet<Category>();
-	
+	@ManyToMany
+	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = { @JoinColumn(name = "role_id") })
+	private Set<Role> roles = new HashSet<Role>();
+
 	public void addCategory(Category category)
 	{
 		category.setUser(this);
-		categories.add(category);		
+		categories.add(category);
 	}
 }
