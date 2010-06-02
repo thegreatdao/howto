fc91072e2ee12ee89b6294d5d6d387f4bff5125d
HL <anotherdayinpara@yahoo.com>
2010-06-02 16:50:43 -0400

    added component class

---
+++ src/main/java/training/hl/bean/hibernate/Profile.java
@@ -0,0 +1,11 @@
+package training.hl.bean.hibernate;
+
+import lombok.Data;
+
+@Data
+public class Profile
+{
+	private String bio;
+	private String hobbies;
+	private String homePage;
+}
--- src/main/java/training/hl/bean/hibernate/User.java
+++ src/main/java/training/hl/bean/hibernate/User.java
@@ -47,7 +47,7 @@ public class User extends RootEntity
 	@ManyToMany
 	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = { @JoinColumn(name = "role_id") })
 	private Set<Role> roles = new HashSet<Role>();
-
+	
 	public void addCategory(Category category)
 	{
 		category.setUser(this);
